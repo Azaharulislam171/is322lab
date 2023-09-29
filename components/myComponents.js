28911fac0e47bb684bca1a0f96436caaa2546187
Mohammad Azaharul Islam <Azaharulislam171@users.noreply.github.com>
2023-09-29 09:24:38 -0400

    added a component

---
+++ components/myComponents.js
@@ -0,0 +1,13 @@
+// components/MyComponent.js
+
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <p>This is Azaharul Islam's custom React component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
